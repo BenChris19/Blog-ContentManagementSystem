dbc8a2092c51609df5192ec94075d3904de9af27
BenChris19 <chris.underwood.uk@gmail.com>
2022-09-23 14:58:18 +0100

    SQL

--- DataBaseCreation/blogCMSDB.sql
+++ DataBaseCreation/blogCMSDB.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "ROLE" (
 	roleId SERIAL PRIMARY KEY,
+<<<<<<< Updated upstream
     "role" varchar(30)
 );
 
@@ -11,6 +12,24 @@ CREATE TABLE "USER" (
     lastName varchar(30) not null,
     email varchar(30) not null,
     "enable" boolean not null
+=======
+    roleType VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE HASHTAGS (
+	hashtagId SERIAL PRIMARY KEY,
+    hashtagName VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE USERS (
+	userId SERIAL PRIMARY KEY,
+    userName VARCHAR(30) NOT NULL,
+    userPassword VARCHAR(100) NOT NULL,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    isAdmin BOOLEAN NOT NULL
+>>>>>>> Stashed changes
 );
 
 CREATE TABLE USER_ROLE (
@@ -23,6 +42,7 @@ CREATE TABLE USER_ROLE (
 
 CREATE TABLE BLOGPOST (
 	blogpostId SERIAL PRIMARY KEY,
+<<<<<<< Updated upstream
     timePosted TIMESTAMP with time zone,
     title varchar(100) not null,
     "type" varchar(10) not null,
@@ -41,6 +61,26 @@ CREATE TABLE "COMMENT" (
     FOREIGN KEY (userId) references "USER"(userId),
     blogpostId int,
     FOREIGN KEY (blogpostId) references blogpost (blogpostId)
+=======
+    timePosted TIMESTAMP WITH TIME ZONE,
+    blogTitle VARCHAR(100) NOT NULL,
+    blogType VARCHAR(30) NOT NULL,
+    blogStatus VARCHAR(30) NOT NULL,
+    blogPhoto VARCHAR(255),
+    blogContent TEXT NOT NULL,
+    userId INT,
+    FOREIGN KEY (userId) references USERS(userId)
+);
+
+CREATE TABLE BLOGCOMMENTS (
+	commentId SERIAL PRIMARY KEY,
+    timePosted TIMESTAMP with time zone,
+    commentsContent TEXT NOT NULL,
+    userId INT,
+    FOREIGN KEY (userId) REFERENCES USERS(userId),
+    blogpostId INT,
+    FOREIGN KEY (blogpostId) REFERENCES BLOGPOSTS (blogpostId)
+>>>>>>> Stashed changes
 );
 
 CREATE TABLE HASHTAG (
@@ -49,9 +89,20 @@ CREATE TABLE HASHTAG (
 );
 
 CREATE TABLE BLOGPOST_HASHTAG (
-	blogpostId int,
-    hashtagId int,
+	blogpostId INT,
+    hashtagId INT,
     PRIMARY KEY (blogpostId, hashtagId),
-    FOREIGN KEY (blogpostId) references blogpost (blogpostId),
-    FOREIGN KEY (hashtagId) references hashtag (hashtagId)
+    FOREIGN KEY (blogpostId) REFERENCES BLOGPOSTS (blogpostId),
+    FOREIGN KEY (hashtagId) REFERENCES HASHTAGS (hashtagId)
 );
+<<<<<<< Updated upstream
+=======
+
+CREATE TABLE USER_ROLE (
+	userId INT,
+    roleId INT,
+    PRIMARY KEY (userId, roleId),
+    FOREIGN KEY (userId) REFERENCES USERS(userId),
+    FOREIGN KEY (roleId) REFERENCES ROLES(roleId)
+);
+>>>>>>> Stashed changes
